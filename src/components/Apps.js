aed8fc1b4be39fa9ef96ba275802655863b9912d
KENNETH <kenneth.mburu@student.moringaschool.com>
2023-04-05 17:24:09 +0300

    Update Apps.js

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
     // auto-login
-    fetch("/me").then((r) => {
+    fetch("https://serenity-server.onrender.com/me").then((r) => {
       if (r.ok) {
         r.json().then((user) => setUser(user));
       }
